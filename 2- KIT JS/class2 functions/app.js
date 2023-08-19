4de058559a15ec8932ea23029f0a3d609e5f065c
1shabbirhussain <001.shabbirhussain@gmail.com>
2023-08-19 20:05:30 +0500

    whole folder

---
+++ 2- KIT JS/class2 functions/app.js
@@ -0,0 +1,94 @@
+// function pehlaFunc(){
+//     console.log("abdure rehman bawla")
+//     console.log("function yahan par khtm agey return dega")
+//     return  ;
+
+// }
+
+// const a = pehlaFunc();
+// console.log(a);
+
+
+
+
+// let add2 = ()=>{
+//     let a = 2;
+//     let b = 2;
+//     return a+b;
+// }
+
+// let add1 = ()=> add2;
+
+// let add = () => add1;
+
+// const a = add()()();
+// console.log(a);
+
+
+
+// ----------------------
+
+// tax calculation function 
+
+
+// let calculateProductTax = (product, tax) => {
+//     let total = 0;
+//     let totalProduct = 0;
+//     for (let i = 0; i < product.length; i++) {
+//         total += product[i] + product[i] * tax;
+//         totalProduct += product[i];
+
+//     }
+//     return {
+//         productPriceWithTax: total,
+//         productPriceWithoutTax: totalProduct
+//     }
+// }
+
+// let calculateTax = calculateProductTax([50, 100, 200], 0.2);
+// console.log(calculateTax);
+
+
+// --------------------
+
+
+
+// let calculateProductTax = function(product, tax) {
+//     let total = 0;
+//     let totalProduct = 0;
+//     for (let i = 0; i < product.length; i++) {
+//         total += product[i] + product[i] * tax;
+//         totalProduct += product[i];
+
+//     }
+//     return {
+//         productPriceWithTax: total,
+//         productPriceWithoutTax: totalProduct
+//     }
+// }
+
+// let calculateTax = calculateProductTax([50, 100, 200], 0.2);
+// console.log(calculateTax);
+
+
+// --------------
+
+
+// function calculateProductTax(product, tax) {
+//     let total = 0;
+//     let totalProduct = 0;
+//     for (let i = 0; i < product.length; i++) {
+//         total += product[i] + product[i] * tax;
+//         totalProduct += product[i];
+
+//     }
+//     return {
+//         productPriceWithTax: total,
+//         productPriceWithoutTax: totalProduct
+//     }
+// }
+
+// let calculateTax = calculateProductTax([50, 100, 200], 0.2);
+// console.log(calculateTax);
+
+
